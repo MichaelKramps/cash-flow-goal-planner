c78b0d6cde5ec442596adb6ed7b5f06107e9f06e
Michael Kramps <michaelkramps@carfax.com>
2021-04-26 11:20:32 -0500

    add amortization schedule to loan calculator

--- src/components/Calculators/LoanCalculator.js
+++ src/components/Calculators/LoanCalculator.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './LoanCalculator.css';
+import AmortizationSchedule from "../Shared/AmortizationSchedule";
 
 class LoanCalculator extends React.Component {
 
@@ -10,7 +11,8 @@ class LoanCalculator extends React.Component {
             amount: this.props.price,
             term: this.props.term,
             interestRate: this.props.interestRate,
-            monthlyPayment: 0
+            monthlyPayment: 0,
+            amortization: []
         };
 
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -28,7 +30,7 @@ class LoanCalculator extends React.Component {
     }
 
     handleInterestRateChange(event) {
-        this.setState({interestRate: parseInt(event.target.value)});
+        this.setState({interestRate: parseFloat(event.target.value)});
     }
 
     calculateMonthlyPayments() {
@@ -39,37 +41,69 @@ class LoanCalculator extends React.Component {
         let bottomDivider = periodicInterestRate * Math.pow(1 + periodicInterestRate, numberPayments);
         let denominator = topDivider / bottomDivider;
 
-        return loanAmount / denominator;
+        return (loanAmount / denominator).toFixed(2);
+    }
+
+    createAmortization() {
+        let monthlyPayment = this.state.monthlyPayment;
+        let loanBalance = this.state.amount;
+        let periodicInterestRate = this.state.interestRate / 1200;
+        this.state.amortization = [];
+
+        for(let payment = 1; payment < this.state.term * 12; payment++) {
+            let entry = {};
+            entry.payment = monthlyPayment;
+            entry.interestPaid = (loanBalance * periodicInterestRate).toFixed(2);
+            entry.principalPaid = (monthlyPayment - entry.interestPaid).toFixed(2);
+            loanBalance = (loanBalance - entry.principalPaid).toFixed(2);
+            entry.loanBalance = loanBalance;
+
+            this.state.amortization.push(entry);
+        }
+
+        let lastEntry = {};
+        lastEntry.interestPaid = (loanBalance * periodicInterestRate).toFixed(2);
+        lastEntry.principalPaid = loanBalance;
+        lastEntry.payment = (parseFloat(loanBalance) + parseFloat(lastEntry.interestPaid)).toFixed(2);
+        lastEntry.loanBalance = 0;
+
+        this.state.amortization.push(lastEntry);
+        this.forceUpdate();
     }
 
     handleSubmit(event) {
         event.preventDefault();
-        this.setState({monthlyPayment: this.calculateMonthlyPayments()});
+        this.setState({monthlyPayment: this.calculateMonthlyPayments()}, () => {
+            this.createAmortization();
+        });
     }
 
     render() {
         return (
-            <form className="loan-calculator" onSubmit={this.handleSubmit}>
-                <h2>Loan Calculator</h2>
-                <div>
-                    <label>Loan Amount</label>
-                    <input value={this.state.amount} type="number" onChange={this.handleAmountChange} />
-                </div>
-                <div>
-                    <label>Length of Loan</label>
-                    <input value={this.state.term} type="number" onChange={this.handleTermChange} />
-                </div>
-                <div>
-                    <label>Interest Rate</label>
-                    <input value={this.state.interestRate} type="number" onChange={this.handleInterestRateChange} />
-                </div>
-                <div>
-                    Monthly Payment: ${this.state.monthlyPayment}
-                </div>
-                <div>
-                    <input type="submit" value="Calculate" />
-                </div>
-            </form>
+            <div>
+                <form className="loan-calculator" onSubmit={this.handleSubmit}>
+                    <h2>Loan Calculator</h2>
+                    <div>
+                        <label>Loan Amount</label>
+                        <input value={this.state.amount} type="number" onChange={this.handleAmountChange} />
+                    </div>
+                    <div>
+                        <label>Length of Loan</label>
+                        <input value={this.state.term} type="number" onChange={this.handleTermChange} />
+                    </div>
+                    <div>
+                        <label>Interest Rate</label>
+                        <input value={this.state.interestRate} type="number" onChange={this.handleInterestRateChange} />
+                    </div>
+                    <div>
+                        Monthly Payment: ${this.state.monthlyPayment}
+                    </div>
+                    <div>
+                        <input type="submit" value="Calculate" />
+                    </div>
+                </form>
+                <AmortizationSchedule amortization={this.state.amortization} />
+            </div>
         );
     }
 }
---
+++ src/components/Shared/AmortizationSchedule.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import './AmortizationSchedule.css';
+
+class AmortizationSchedule extends React.Component {
+
+    createRowHeaders() {
+        let firstRow = this.props.amortization[0];
+        if (firstRow.insurance || firstRow.propertyTax || firstRow.hoa) {
+            return (
+
+                <div className="amortization-row">
+                    <div>Payment Number</div>
+                    <div>Payment Amount</div>
+                    <div>Principal Paid</div>
+                    <div>Interest Paid</div>
+                    <div>Property Insurance</div>
+                    <div>Property Taxes</div>
+                    <div>HOA Fee</div>
+                    <div>Loan Balance</div>
+                </div>
+            );
+        } else {
+            return (
+                <div className="amortization-row">
+                    <div>Payment Number</div>
+                    <div>Payment Amount</div>
+                    <div>Principal Paid</div>
+                    <div>Interest Paid</div>
+                    <div>Loan Balance</div>
+                </div>
+            );
+        }
+    }
+
+    createRows() {
+        let firstRow = this.props.amortization[0];
+        if (firstRow.insurance || firstRow.propertyTax || firstRow.hoa) {
+            return this.createMortgageRows();
+        } else {
+            return this.createLoanRows();
+        }
+    }
+
+    createLoanRows() {
+        return this.props.amortization.map((month, index) => {
+            return (
+                <div className="amortization-row">
+                    <div>{index + 1}</div>
+                    <div>${month.payment}</div>
+                    <div>${month.principalPaid}</div>
+                    <div>${month.interestPaid}</div>
+                    <div>${month.loanBalance}</div>
+                </div>
+            )
+        })
+    }
+
+    createMortgageRows() {
+        return this.props.amortization.map((month, index) => {
+            return (
+                <div className="amortization-row">
+                    <div>Payment Number</div>
+                    <div>Payment Amount</div>
+                    <div>Principal Paid</div>
+                    <div>Interest Paid</div>
+                    <div>Property Insurance</div>
+                    <div>Property Taxes</div>
+                    <div>HOA Fee</div>
+                    <div>Loan Balance</div>
+                </div>
+            );
+        })
+    }
+
+    render() {
+        if (this.props.amortization.length === 0) {
+            return null;
+        } else {
+            return (
+                <div className="amortization-schedule">
+                    {this.createRowHeaders()}
+                    {this.createRows()}
+                </div>
+            );
+        }
+    }
+}
+
+export default AmortizationSchedule;
