c8da8433e8e7080fc3b9b442c229439b1d12e674
Michael Kramps <michaelkramps@carfax.com>
2021-04-28 15:41:52 -0500

    use improved form validation on mortgage calculator

--- src/components/Calculators/MortgageCalculator.js
+++ src/components/Calculators/MortgageCalculator.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './MortgageCalculator.css';
+import FormUtils from "../Shared/FormUtils";
 
 class MortgageCalculator extends React.Component {
 
@@ -28,31 +29,31 @@ class MortgageCalculator extends React.Component {
     }
 
     handlePriceChange(event) {
-        this.setState({price: parseInt(event.target.value)});
+        this.setState({price: event.target.value});
     }
 
     handleDownPaymentChange(event) {
-        this.setState({downPayment: parseInt(event.target.value)});
+        this.setState({downPayment: event.target.value});
     }
 
     handleTermChange(event) {
-        this.setState({term: parseInt(event.target.value)});
+        this.setState({term: event.target.value});
     }
 
     handleInterestRateChange(event) {
-        this.setState({interestRate: parseInt(event.target.value)});
+        this.setState({interestRate: event.target.value});
     }
 
     handleInsuranceChange(event) {
-        this.setState({insurance: parseInt(event.target.value)});
+        this.setState({insurance: event.target.value});
     }
 
     handlePropertyTaxChange(event) {
-        this.setState({propertyTax: parseInt(event.target.value)});
+        this.setState({propertyTax: event.target.value});
     }
 
     handleHoaChange(event) {
-        this.setState({Hoa: parseInt(event.target.value)});
+        this.setState({Hoa: event.target.value});
     }
 
     calculateMonthlyPayments() {
@@ -63,7 +64,7 @@ class MortgageCalculator extends React.Component {
         let bottomDivider = periodicInterestRate * Math.pow(1 + periodicInterestRate, numberPayments);
         let denominator = topDivider / bottomDivider;
 
-        return loanAmount / denominator;
+        return (loanAmount / denominator).toFixed(2);
     }
 
     handleSubmit(event) {
@@ -77,31 +78,59 @@ class MortgageCalculator extends React.Component {
                 <h2>Mortgage Calculator</h2>
                 <div>
                     <label>Price of the home</label>
-                    <input value={this.state.price} type="number" onChange={this.handlePriceChange} />
+                    <input
+                        value={this.state.price}
+                        onKeyDown={(event) => {FormUtils.validateIntegerInput(event)}}
+                        onChange={this.handlePriceChange}
+                    />
                 </div>
                 <div>
                     <label>Down Payment</label>
-                    <input value={this.state.downPayment} type="number" onChange={this.handleDownPaymentChange} />
+                    <input
+                        value={this.state.downPayment}
+                        onKeyDown={(event) => {FormUtils.validateIntegerInput(event)}}
+                        onChange={this.handleDownPaymentChange}
+                    />
                 </div>
                 <div>
                     <label>Length of Loan</label>
-                    <input value={this.state.term} type="number" onChange={this.handleTermChange} />
+                    <input
+                        value={this.state.term}
+                        onKeyDown={(event) => {FormUtils.validateIntegerInput(event)}}
+                        onChange={this.handleTermChange}
+                    />
                 </div>
                 <div>
                     <label>Interest Rate</label>
-                    <input value={this.state.interestRate} type="number" onChange={this.handleInterestRateChange} />
+                    <input
+                        value={this.state.interestRate}
+                        onKeyDown={(event) => {FormUtils.validateFloatInput(event, this.state.interestRate)}}
+                        onChange={this.handleInterestRateChange}
+                    />
                 </div>
                 <div>
                     <label>Home Insurance</label>
-                    <input value={this.state.insurance} type="number" onChange={this.handleInsuranceChange} />
+                    <input
+                        value={this.state.insurance}
+                        onKeyDown={(event) => {FormUtils.validateIntegerInput(event)}}
+                        onChange={this.handleInsuranceChange}
+                    />
                 </div>
                 <div>
                     <label>Property Taxes</label>
-                    <input value={this.state.propertyTax} type="number" onChange={this.handlePropertyTaxChange} />
+                    <input
+                        value={this.state.propertyTax}
+                        onKeyDown={(event) => {FormUtils.validateIntegerInput(event)}}
+                        onChange={this.handlePropertyTaxChange}
+                    />
                 </div>
                 <div>
                     <label>Home Owner's Association Fee</label>
-                    <input value={this.state.hoa} type="number" onChange={this.handleHoaChange} />
+                    <input
+                        value={this.state.hoa}
+                        onKeyDown={(event) => {FormUtils.validateIntegerInput(event)}}
+                        onChange={this.handleHoaChange}
+                    />
                 </div>
                 <div>
                     Monthly Payment: ${this.state.monthlyPayment}
