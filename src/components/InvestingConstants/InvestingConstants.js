5a11e813605bfd041c5e56b16af8e17ff0e74c0e
Michael Kramps <michaelkramps@carfax.com>
2021-05-23 14:32:04 -0500

    add some ideas for investing constants

--- src/components/InvestingConstants/InvestingConstants.js
+++ src/components/InvestingConstants/InvestingConstants.js
@@ -8,6 +8,13 @@ class InvestingConstants extends React.Component {
         return (
             <div className={"investing-constants " + Shared.determineVisibility(this.props)}>
                 Investing Constants
+                Start with U.S. only? then try to add other countries...
+                <ul>
+                    <li>real estate appreciation</li>
+                    <li>inflation rate</li>
+                    <li>average stock market return</li>
+                    <li>base loan interest rate</li>
+                </ul>
             </div>
         );
     }
