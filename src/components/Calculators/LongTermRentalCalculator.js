487887816d2f6425bb2aee5052a33d1eb97dd657
Michael Kramps <michaelkramps@carfax.com>
2021-05-29 20:46:07 -0500

    add initial investment fields to long term rental calculator

--- src/components/Calculators/LongTermRentalCalculator.js
+++ src/components/Calculators/LongTermRentalCalculator.js
@@ -6,6 +6,11 @@ import FormUtils from "../Shared/FormUtils";
 class LongTermRentalCalculator extends React.Component {
 
     defaultState = {
+        propertyValue: this.props.propertyValue || 0,
+        downPayment: this.props.downPayment || 0,
+        renovationCosts: this.props.renovationCosts || 0,
+        holdingCosts: this.props.holdingCosts || 0,
+        otherInitialCosts: this.props.otherInitialCosts || 0,
         monthlyIncome: this.props.monthlyIncome || 0,
         mortgage: this.props.mortgage || 0,
         utilities: this.props.utilities || 0,
@@ -22,6 +27,11 @@ class LongTermRentalCalculator extends React.Component {
         this.state = this.defaultState;
 
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handlePropertyValueChange = this.handlePropertyValueChange.bind(this);
+        this.handleDownPaymentChange = this.handleDownPaymentChange.bind(this);
+        this.handleRenovationCostsChange = this.handleRenovationCostsChange.bind(this);
+        this.handleHoldingCostsChange = this.handleHoldingCostsChange.bind(this);
+        this.handleOtherInitialCostsChange = this.handleOtherInitialCostsChange.bind(this);
         this.handleMonthlyIncomeChange = this.handleMonthlyIncomeChange.bind(this);
         this.handleMortgageChange = this.handleMortgageChange.bind(this);
         this.handleUtilitiesChange = this.handleUtilitiesChange.bind(this);
@@ -31,46 +41,85 @@ class LongTermRentalCalculator extends React.Component {
         this.handleOtherExpensesChange = this.handleOtherExpensesChange.bind(this);
     }
 
+    handlePropertyValueChange(event) {
+        this.setState({propertyValue: event.target.value}, () => {
+            this.updateDynamicFields();
+        });
+    }
+
+    handleDownPaymentChange(event) {
+        this.setState({downPayment: event.target.value}, () => {
+            this.updateDynamicFields();
+        });
+    }
+
+    handleRenovationCostsChange(event) {
+        this.setState({renovationCosts: event.target.value}, () => {
+            this.updateDynamicFields();
+        });
+    }
+
+    handleHoldingCostsChange(event) {
+        this.setState({holdingCosts: event.target.value}, () => {
+            this.updateDynamicFields();
+        });
+    }
+
+    handleOtherInitialCostsChange(event) {
+        this.setState({otherInitialCosts: event.target.value}, () => {
+            this.updateDynamicFields();
+        });
+    }
+
     handleMonthlyIncomeChange(event) {
         this.setState({monthlyIncome: event.target.value}, () => {
-            this.updateCashFlow();
+            this.updateDynamicFields();
         });
     }
 
     handleMortgageChange(event) {
         this.setState({mortgage: event.target.value}, () => {
-            this.updateCashFlow();
+            this.updateDynamicFields();
         });
     }
 
     handleUtilitiesChange(event) {
         this.setState({utilities: event.target.value}, () => {
-            this.updateCashFlow();
+            this.updateDynamicFields();
         });
     }
 
     handleMaintenanceChange(event) {
         this.setState({maintenance: event.target.value}, () => {
-            this.updateCashFlow();
+            this.updateDynamicFields();
         });
     }
 
     handleVacancyChange(event) {
         this.setState({vacancy: event.target.value}, () => {
-            this.updateCashFlow();
+            this.updateDynamicFields();
         });
     }
 
     handleManagementChange(event) {
         this.setState({management: event.target.value}, () => {
-            this.updateCashFlow();
+            this.updateDynamicFields();
         });
     }
 
     handleOtherExpensesChange(event) {
         this.setState({otherExpenses: event.target.value}, () => {
-            this.updateCashFlow();
+            this.updateDynamicFields();
         });
+    }
+
+    updateInitialInvestment() {
+        let initialInvestment = FormUtils.parseIntegerInput(this.state.downPayment);
+        initialInvestment += FormUtils.parseIntegerInput(this.state.renovationCosts);
+        initialInvestment += FormUtils.parseIntegerInput(this.state.holdingCosts);
+        initialInvestment += FormUtils.parseIntegerInput(this.state.otherInitialCosts);
+
+        this.setState({initialInvestment: initialInvestment});
     }
 
     updateCashFlow() {
@@ -83,6 +132,11 @@ class LongTermRentalCalculator extends React.Component {
         newCashFlow -= FormUtils.parseIntegerInput(this.state.otherExpenses);
 
         this.setState({cashFlow: newCashFlow});
+    }
+
+    updateDynamicFields() {
+        this.updateCashFlow();
+        this.updateInitialInvestment();
     }
 
     handleSubmit(event) {
@@ -102,6 +156,51 @@ class LongTermRentalCalculator extends React.Component {
             <div className={"long-term-rental-calculator " + Shared.determineVisibility(this.props)}>
                 <form onSubmit={this.handleSubmit}>
                     <h2>Long Term Rental Calculator</h2>
+                    <h3>Initial Investment</h3>
+                    <div>
+                        <label>Purchase Price/Property Value</label>
+                        <input
+                            value={this.state.propertyValue}
+                            onKeyDown={(event) => {FormUtils.validateIntegerInput(event)}}
+                            onChange={this.handlePropertyValueChange}
+                        />
+                    </div>
+                    <div>
+                        <label>Down Payment</label>
+                        <input
+                            value={this.state.downPayment}
+                            onKeyDown={(event) => {FormUtils.validateIntegerInput(event)}}
+                            onChange={this.handleDownPaymentChange}
+                        />
+                    </div>
+                    <div>
+                        <label>Renovation Costs</label>
+                        <input
+                            value={this.state.renovationCosts}
+                            onKeyDown={(event) => {FormUtils.validateIntegerInput(event)}}
+                            onChange={this.handleRenovationCostsChange}
+                        />
+                    </div>
+                    <div>
+                        <label>Holding Costs</label>
+                        <input
+                            value={this.state.holdingCosts}
+                            onKeyDown={(event) => {FormUtils.validateIntegerInput(event)}}
+                            onChange={this.handleHoldingCostsChange}
+                        />
+                    </div>
+                    <div>
+                        <label>Other Initial Costs</label>
+                        <input
+                            value={this.state.otherInitialCosts}
+                            onKeyDown={(event) => {FormUtils.validateIntegerInput(event)}}
+                            onChange={this.handleOtherInitialCostsChange}
+                        />
+                    </div>
+                    <div>
+                        Total Initial Investment: ${this.state.initialInvestment}
+                    </div>
+                    <h3>Revenue</h3>
                     <div>
                         <label>Expected Monthly Income</label>
                         <input
