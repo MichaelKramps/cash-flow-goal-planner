1db9c4af97687724d4e2791992ac80a5250979c2
Michael Kramps <michaelkramps@carfax.com>
2021-06-02 15:58:00 -0500

    dumb implementation of future cash flow outlook

--- src/components/Outlook/CashFlowOutlook.js
+++ src/components/Outlook/CashFlowOutlook.js
@@ -16,6 +16,73 @@ class CashFlowOutlook extends React.Component {
         return yearsArray;
     }
 
+    listAllAssets() {
+        let listOfExistingAssetsWithCashFlow = this.createListOfExistingAssetsWithCashFlow();
+        let listOfFutureAssetsWithCashFlow = this.createListOfFutureAssetsWithCashFlow();
+        let allAssetsWithFutureCashFlow = listOfExistingAssetsWithCashFlow.concat(listOfFutureAssetsWithCashFlow);
+
+        return allAssetsWithFutureCashFlow.map((asset) => {
+            return (
+                <tr key={asset.name}>
+                    <td>{asset.name}</td>
+                    <td>${asset.cashFlow[0]}</td>
+                    <td>${asset.cashFlow[1]}</td>
+                    <td>${asset.cashFlow[2]}</td>
+                    <td>${asset.cashFlow[3]}</td>
+                    <td>${asset.cashFlow[4]}</td>
+                </tr>
+            )
+        })
+    }
+
+    createListOfExistingAssetsWithCashFlow() {
+        let existingAssetsWithCashFlow = [];
+        let rawExistingAssets = this.props.highlights.currentCashFlow ? this.props.highlights.currentCashFlow.currentAssets : [];
+
+        for (let index = 0; index < rawExistingAssets.length; index++) {
+            existingAssetsWithCashFlow.push(this.createCurrentAssetWithCashFlow(rawExistingAssets[index]));
+        }
+
+        return existingAssetsWithCashFlow;
+    }
+
+    createCurrentAssetWithCashFlow(currentAsset) {
+        let assetWithCashFlow = {};
+        assetWithCashFlow.name = currentAsset.name;
+        assetWithCashFlow.cashFlow = [];
+
+        for (let year = 0; year < 5; year++) {
+            let thisSegmentsCashFlow = (currentAsset.cashFlow * Math.pow(1.03, year)).toFixed(2);
+            assetWithCashFlow.cashFlow.push(thisSegmentsCashFlow)
+        }
+
+        return assetWithCashFlow;
+    }
+
+    createListOfFutureAssetsWithCashFlow() {
+        let futureAssetsWithCashFlow = [];
+        let rawFutureAssets = this.props.futureAssets.futureAssets ? this.props.futureAssets.futureAssets : [];
+
+        for (let index = 0; index < rawFutureAssets.length; index++) {
+            futureAssetsWithCashFlow.push(this.createFutureAssetWithCashFlow(rawFutureAssets[index]));
+        }
+
+        return futureAssetsWithCashFlow;
+    }
+
+    createFutureAssetWithCashFlow(futureAsset) {
+        let assetWithCashFlow = {};
+        assetWithCashFlow.name = futureAsset.name;
+        assetWithCashFlow.cashFlow = [];
+
+        for (let year = 0; year < 5; year++) {
+            let thisSegmentsCashFlow = (futureAsset.cashFlow * Math.pow(1.03, year)).toFixed(2);
+            assetWithCashFlow.cashFlow.push(thisSegmentsCashFlow)
+        }
+
+        return assetWithCashFlow;
+    }
+
     render() {
         let years = this.createYearsArray();
         return (
@@ -31,14 +98,7 @@ class CashFlowOutlook extends React.Component {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>asset 1</td>
-                        <td>cash flow asset 1 year 1</td>
-                        <td>cash flow asset 1 year 2</td>
-                        <td>cash flow asset 1 year 3</td>
-                        <td>cash flow asset 1 year 4</td>
-                        <td>cash flow asset 1 year 5</td>
-                    </tr>
+                    {this.listAllAssets()}
                     <tr>
                         <td>Total</td>
                         <td>total cash flow year 1</td>
