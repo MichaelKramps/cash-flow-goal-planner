b53a01a1a00d71702acbba76c150e54d5baae8a4
Michael Kramps <michaelkramps@carfax.com>
2021-07-08 21:10:54 -0500

    add steps one and two

--- src/components/NextSteps/Steps/StepOne.js
+++ src/components/NextSteps/Steps/StepOne.js
@@ -6,7 +6,14 @@ class StepOne extends React.Component {
         return (
             <div className={"step-one " + Shared.determineVisibility(this.props)}>
                 <h2>Step One: Enter Your Cash Flow Goal</h2>
-
+                <p>First, you should head over to the cash flow planner and edit your cash flow goal. The cash flow planner will help you create a cash flow goal based on your monthly expenses and investing goals.</p>
+                <p>In order to achieve financial freedom you must know exactly how much money you need to make. And you should know exactly when you want to get there. Most retirement plans have you aim for a particular net worth, but the simplest way to set a retirement/financial freedom goal is by looking at your monthly cash flow.</p>
+                <h3>Determine Your Monthly Expenses</h3>
+                <p>When you create a budget, you typically categorize your expenses. It helps visualize where your money is going and highlight opportunities to improve your spending.</p>
+                <p>The planner should be treated like a future budget. Not the budget that you have today, but the one you'd like to have in the future. Part of this future budget includes what you'd like to be able to spend on yourself, on things like shopping and travelling.</p>
+                <h3>Create Room For Future Investing</h3>
+                <p>I believe you should continue investing even when you reach your financial goal.</p>
+                <p>Why? Because the price of goods and services increases over time. The force of inflation guarantees that money has less buying power over time. By continuing to invest you can ensure that your cash flow continues to increase to offset inflation.</p>
             </div>
         )
     }
--- src/components/NextSteps/Steps/StepTwo.js
+++ src/components/NextSteps/Steps/StepTwo.js
@@ -6,6 +6,8 @@ class StepTwo extends React.Component {
         return (
             <div className={"step-two " + Shared.determineVisibility(this.props)}>
                 <h2>Step Two: Add Your Current Assets</h2>
+                <p>Add any investments you already have to the cash flow planner. If you already have a stock portfolio, some real estate or some other cash flow producing asset, add it to the list.</p>
+                <p>Your cash flow goal determines how much cash flow your investments need to produce in order to support the life you want. But many of us aren't starting from zero. Adding your current assets will show how much more cash flow you need to create in order to reach your goal.</p>
             </div>
         )
     }
