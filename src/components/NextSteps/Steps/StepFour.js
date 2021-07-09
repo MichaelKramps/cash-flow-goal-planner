4e92b8c87ce43c16ffac61cdbec585fed8cbe85d
Michael Kramps <michaelkramps@carfax.com>
2021-07-09 16:02:28 -0500

    working on step four

--- src/components/NextSteps/Steps/StepFour.js
+++ src/components/NextSteps/Steps/StepFour.js
@@ -6,6 +6,7 @@ class StepFour extends React.Component {
         return (
             <div className={"step-four " + Shared.determineVisibility(this.props)}>
                 <h2>Step Four: Work Towards Buying Your Next Investment</h2>
+                <p>I've made road maps for the most common types of cash flowing investments to help walk you through every actionable step. Following these guides can take you from knowing absolutely nothing to owning a cash flowing asset.</p>
             </div>
         )
     }
