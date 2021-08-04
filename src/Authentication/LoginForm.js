949b4120b50797afa4fd81780dd4d4a6b889abc6
Michael Kramps <michaelkramps@carfax.com>
2021-08-04 14:40:42 -0500

    improve ux of the login form

--- src/Authentication/LoginForm.js
+++ src/Authentication/LoginForm.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Authentication from "./Authentication";
+import './LoginForm.css';
 
 class LoginForm extends React.Component {
 
@@ -21,6 +22,7 @@ class LoginForm extends React.Component {
         this.handleSignIn = this.handleSignIn.bind(this);
         this.handleConfirmSignUp = this.handleConfirmSignUp.bind(this);
         this.resendCode = this.resendCode.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
     }
 
     setView(view){
@@ -29,6 +31,13 @@ class LoginForm extends React.Component {
 
     determineVisible(view) {
         return view === this.state.view ? "visible" : "invisible";
+    }
+
+    printError() {
+        if (this.state.error) {
+            return <p className="login-error">*{this.state.error}</p>;
+        }
+        return null;
     }
 
     handleEmailChange(event) {
@@ -44,6 +53,7 @@ class LoginForm extends React.Component {
     }
 
     async handleSignUp() {
+        this.setState({error: null});
         try {
             await Authentication.signUp(this.state.email, this.state.password)
             this.setView("confirm-sign-up");
@@ -65,61 +75,94 @@ class LoginForm extends React.Component {
     }
 
     async handleSignIn() {
+        this.setState({error: null});
         try {
             await Authentication.signIn(this.state.email, this.state.password)
-            this.setView("confirm-sign-in");
         } catch (error) {
             this.setState({error: error.message});
             if (error.code === "UserNotConfirmedException") {
-                this.setView("confirm-sign-in");
+                this.setView("confirm-sign-up");
             }
             console.log("error signing in:", error);
         }
     }
 
+    async onSubmit(e) {
+        e.preventDefault();
+        if (this.state.view === "sign-in") {
+            await this.handleSignIn();
+        } else if (this.state.view === "sign-up") {
+            await this.handleSignUp();
+        } else if (this.state.view === "confirm-sign-up") {
+            await this.handleConfirmSignUp();
+        }
+    }
+
     render() {
         return (
-            <React.Fragment>
+            <form className="login-container" onSubmit={this.onSubmit}>
                 <div className={this.determineVisible("sign-in")}>
                     <h2>Sign in</h2>
-                    <label>Email: </label>
-                    <input
-                        value={this.state.email}
-                        onChange={this.handleEmailChange}
-                    />
-                    <label>Password: </label>
-                    <input
-                        value={this.state.password}
-                        onChange={this.handlePasswordChange}
-                    />
-                    <a href="#" onClick={(e) => {e.preventDefault(); this.setView("sign-up")}}>Sign up</a>
+                    {this.printError()}
+                    <div className="login-input-container">
+                        <label>Email: </label>
+                        <input
+                            value={this.state.email}
+                            onChange={this.handleEmailChange}
+                        />
+                    </div>
+                    <div className="login-input-container">
+                        <label>Password: </label>
+                        <input
+                            value={this.state.password}
+                            type="password"
+                            onChange={this.handlePasswordChange}
+                        />
+                    </div>
+                    <p>
+                        Don't have an account?&nbsp;
+                        <a href="#" onClick={(e) => {e.preventDefault(); this.setView("sign-up")}}>Sign up</a>
+                    </p>
                     <button onClick={this.handleSignIn}>Sign in</button>
                 </div>
                 <div className={this.determineVisible("sign-up")}>
                     <h2>Sign up</h2>
-                    <label>Email: </label>
-                    <input
-                        value={this.state.email}
-                        onChange={this.handleEmailChange}
-                    />
-                    <label>Password: </label>
-                    <input
-                        value={this.state.password}
-                        onChange={this.handlePasswordChange}
-                    />
-                    <a href="#" onClick={(e) => {e.preventDefault(); this.setView("sign-in")}}>Sign in</a>
+                    {this.printError()}
+                    <div className="login-input-container">
+                        <label>Email: </label>
+                        <input
+                            value={this.state.email}
+                            onChange={this.handleEmailChange}
+                        />
+                    </div>
+                    <div className="login-input-container">
+                        <label>Password: </label>
+                        <input
+                            value={this.state.password}
+                            type="password"
+                            onChange={this.handlePasswordChange}
+                        />
+                    </div>
+                    <p>
+                        Already have an account?&nbsp;
+                        <a href="#" onClick={(e) => {e.preventDefault(); this.setView("sign-in")}}>Sign in</a>
+                    </p>
                     <button onClick={this.handleSignUp}>Sign up</button>
                 </div>
                 <div className={this.determineVisible("confirm-sign-up")}>
-                    <label>Confirmation code: </label>
-                    <input
-                        value={this.state.confirmationCode}
-                        onChange={this.handleConfirmationCodeChange}
-                    />
+                    <h2>Confirm your email</h2>
+                    {this.printError()}
+                    <div className="login-input-container">
+                        <label>Confirmation code: </label>
+                        <input
+                            value={this.state.confirmationCode}
+                            onChange={this.handleConfirmationCodeChange}
+                        />
+                    </div>
                     <a href="#" onClick={this.resendCode}>Send code again</a>
                     <button onClick={this.handleConfirmSignUp}>Confirm code</button>
                 </div>
-            </React.Fragment>
+            </form>
         )
     }
 }
