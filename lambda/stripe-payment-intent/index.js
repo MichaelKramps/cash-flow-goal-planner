35c27da6b9c22ec62da3a0b65e8a4cb88bd990b2
Michael Kramps <michaelkramps@carfax.com>
2021-08-17 11:32:42 -0500

    properly format the lambda response

--- lambda/stripe-payment-intent/index.js
+++ lambda/stripe-payment-intent/index.js
@@ -8,15 +8,15 @@ exports.handler = async (event) => {
     });
 
     const response = {
-        statusCode: 200,
-        headers: {
+        "statusCode": 200,
+        "headers": {
             "Access-Control-Allow-Headers" : "Content-Type",
             "Access-Control-Allow-Origin": "http://localhost:3000",
             "Access-Control-Allow-Methods": "POST"
         },
-        body: {
+        "body": JSON.stringify({
             clientSecret: paymentIntent.client_secret
-        }
+        })
     };
 
     return response;
